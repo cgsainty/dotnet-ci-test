5310cdc4a33e0a1ca8c87efb9037f60c40b8f3de
Chris Sainty <cgsainty@gmail.com>
2016-02-21 12:54:42 +0000

    Added DNX test project

---
+++ TestApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using TestApp.Controllers;
+using Xunit;
+
+namespace TestApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void DoesExist()
+        {
+            var controller = new HomeController();
+
+            Assert.NotNull(controller);
+        }
+    }
+}
